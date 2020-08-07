4c6ffd6fecdb02d60cea3e391b87dbe98e21e755
Jakob Henning Jensen <jakobhenning.jensen@magasin.dk>
2020-08-07 14:25:46 +0200

    added main

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import os
-import sys
+new_name = input("What is the name of the project?: ")
+input(f"Press 'Enter' when you have created a repo on git with the name {new_name}: ")
+git_url = f"git@github.com:JakobHenning/{new_name}"
+os.system(f"git remove set-url origin {git_url}")
+os.rename("Create_python_project",new_name) #Rename current repo
+
+
+
+
